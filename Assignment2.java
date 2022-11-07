07207a675197e5eb9bb22303de272e448e0b3271
naman <79268599+namanmehra-cell@users.noreply.github.com>
2022-11-07 14:29:22 +0530

    Update Assignment2.java

--- Assignment2.java
+++ Assignment2.java
@@ -23,10 +23,7 @@ public static int minDays(int samDaily,int kettyDaily, int diff) {
     
     public static void main(String[] args) {
         
-        int difference=4;
-        int samDaily=3;
-        int kettyDaily=5;
-        System.out.println(minDays(samDaily,kettyDaily,difference));
+        System.out.println(minDays(3,5,2));
     }
 
 
